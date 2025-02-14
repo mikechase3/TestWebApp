3b7025a4920dfa1d940665583ea081d9f31d8958
Michael Chase Jr <protectmikechase@gmail.com>
2025-02-14 06:58:19 -0500

    Our website v0.1 <3

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Hello, Streamlit!')
+st.write("Welcome to your first Streamlit app. It's by Annie and Mike Chase who love each other very much and he will marry me someday<3")
